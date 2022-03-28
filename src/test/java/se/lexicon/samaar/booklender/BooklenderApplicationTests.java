4adbe13cd76f875ffe87e395ce31d8a983e8b596
samaarahmed <m.samaar@gmail.com>
2022-03-28 10:06:32 +0200

    springboot up and running

---
+++ src/test/java/se/lexicon/samaar/booklender/BooklenderApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.samaar.booklender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooklenderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
